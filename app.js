c44b2be279125783cd4612dcabaa1e7e7b9a2727
sunakshi96 <sunakshitejwani@gmail.com>
2020-02-26 20:00:22 +0530

    Get summary in Spanish

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const request = require("request");
 
-const url = 'https://api.darksky.net/forecast/42f4f0b7f630aa1fff073d24ef993013/37.8267,-122.4233?units=si';
+const url = 'https://api.darksky.net/forecast/42f4f0b7f630aa1fff073d24ef993013/37.8267,-122.4233?units=si&lang=es';
 
 request({ url: url, json: true}, (error,response) => {
     let temp = response.body.currently.temperature;
     let rainProb = response.body.currently.precipProbability;
-    console.log("It is currently ", temp, " degrees out. There is a ",rainProb,"% chance of rain.");
+    console.log(response.body.daily.data[0].summary," It is currently ", temp, " degrees out. There is a ",rainProb,"% chance of rain.");
 
 
 })
