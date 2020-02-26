86c32f8dd4cd124120240761443512ecf83bcef1
sunakshi96 <sunakshitejwani@gmail.com>
2020-02-26 19:26:40 +0530

    Parse data and get body of current city

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const request = require("request");
 const url = 'https://api.darksky.net/forecast/42f4f0b7f630aa1fff073d24ef993013/37.8267,-122.4233';
 
 request({ url: url}, (error,response) => {
-    console.log(response);
+    const data = JSON.parse(response.body);
+    console.log(data.currently);
 })
