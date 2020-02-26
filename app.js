5cf0ad6dd61c34d46cabc46428152f434fef1832
sunakshi96 <sunakshitejwani@gmail.com>
2020-02-26 19:31:28 +0530

    Use json property of request to automatically parse to JSON object

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require("request");
 
 const url = 'https://api.darksky.net/forecast/42f4f0b7f630aa1fff073d24ef993013/37.8267,-122.4233';
 
-request({ url: url}, (error,response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.currently);
+request({ url: url, json: true}, (error,response) => {
+    console.log(response.body.currently);
 })
