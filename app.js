0073c236154edacd2af7c7aba9678008a6b89560
sunakshi96 <sunakshitejwani@gmail.com>
2020-02-26 19:55:14 +0530

    set units to SI

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require("request");
 
-const url = 'https://api.darksky.net/forecast/42f4f0b7f630aa1fff073d24ef993013/37.8267,-122.4233?key=value&otherKey=otherValue';
+const url = 'https://api.darksky.net/forecast/42f4f0b7f630aa1fff073d24ef993013/37.8267,-122.4233?units=si';
 
 request({ url: url, json: true}, (error,response) => {
     let temp = response.body.currently.temperature;
