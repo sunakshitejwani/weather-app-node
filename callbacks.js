32a021f53d87fb2450af76cb6cd0f233706ff65d
sunakshi96 <sunakshitejwani@gmail.com>
2020-02-28 01:20:02 +0530

    Implement  the Add callback function, using async function setTimeOut

--- callbacks.js
+++ callbacks.js
@@ -1,18 +1,17 @@
-const names = ['Oni', 'Emma', 'Minato'];
-const shortNames = names.filter((names) => {
-    return names.length <= 4;
-});
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
 
-const geocode = (address, callback) => {
-   setTimeout(() => {
-    const data = {
-        latitude:0,
-        longitude:0
-    }
-    callback(data) // send data the callback, and then callback is called in geocode
-   }, 2000)
+const add = (num1, num2, callback) => {
+    setTimeout(() => {
+        let sum = num1 + num2;
+        callback(sum);
+    }, 2000);
 }
 
-geocode('Philadelphia', (data) => {
-    console.log(data);
-}); // geocode is not going to have direct return value
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
